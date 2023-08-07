e438a755d07b9bada48824d08354e71d24455032
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-07 18:31:00 +0300

    django command to wait for the db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+django command to wait for the database to be availabel.
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """ django wait for the database"""
+    def handel(self, *args, **options):
+        pass
