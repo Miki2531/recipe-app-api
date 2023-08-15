66d35d57b4a3b4138be245aec8f0eb6157d66efe
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-15 21:14:01 +0300

    fix type error

--- user/serializers.py
+++ user/serializers.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     """ Serializer for the user object."""
     class Meta:
-        get_user_model()
+        model = get_user_model()
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length':5}}
     def create(self, validated_data):
