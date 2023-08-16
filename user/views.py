5ffea669598282469a810ab18331148a148ed9b8
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-16 16:43:48 +0300

    manage the authentication user.

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 """ 
 Test for user API.
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
@@ -14,4 +14,12 @@ class createTokenView(ObtainAuthToken):
     """ Create a new token for user."""
     serializer_class = AuthTokenSerializer
     serializer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """ Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+
+    """Allows access only to authenticated users."""
+    permission_classes = [permissions.IsAuthenticated]
 
