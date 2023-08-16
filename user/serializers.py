5d66536225f67878ad6b49129567a2a2ff7d6279
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-16 12:38:49 +0300

    Serializer for the user auth token

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,12 @@
 """ 
 Serializers for user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model, 
+    authenticate,
+    )
+
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,3 +18,28 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """ Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+    
+class AuthTokenSerializer(serializers.Serializer):
+        """ Serializer for the user auth token."""
+        email = serializers.EmailField()
+        password = serializers.CharField(
+            style = {'input_type': 'password'},
+            trim_whitspace = False,
+        )
+
+        def validate(self, attrs):
+            """Validate and authenticate the user."""
+            email = attrs.get('email')
+            password = attrs.get('password')
+            """If the given credentials are valid, return a User object."""
+            user = authenticate(
+                request=self.context.get('request'),
+                username=email,
+                password=password,
+
+            )
+            if not user:
+                 msg= _('Unable to authenticate with the provided credentials.')
+                 raise serializers.ValidationError(msg, code='authorization')
+            attrs['user'] = user
+            return attrs
