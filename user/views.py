31a5c2f19d82c453cec84ce589d869db0c35d8e9
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-16 12:39:31 +0300

    Create a new token for user.

--- user/views.py
+++ user/views.py
@@ -2,9 +2,16 @@
 Test for user API.
 """
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """ Create a new user in the system. """
     serializer_class  = UserSerializer
+
+class createTokenView(ObtainAuthToken):
+    """ Create a new token for user."""
+    serializer_class = AuthTokenSerializer
+    serializer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
