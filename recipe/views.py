8192989a1f956afb716fb0e42545c60d4c2e5b35
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-21 22:21:15 +0300

    create a new user recipe

--- recipe/views.py
+++ recipe/views.py
@@ -8,7 +8,7 @@ from recipe import serializers
 class RecipeViewSets(viewsets.ModelViewSet):
     """view for manage recipe APIs."""
 
-    serializer_class = serializers.RecipeSerializers
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -16,4 +16,15 @@ class RecipeViewSets(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrive recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Returns the class that should be used for the serializer."""
+        if self.action == 'list':
+            return serializers.RecipeSerializers
+        
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new user recipe."""
+        serializer.save(user=self.request.user)
 
