9b6a95809b6c9117b12863ecfdb5dba01c554d37
Miki2531 <mikiyasshemelis46@gmail.com>
2023-09-13 17:58:09 +0300

    filter queryset assigend those recipes

--- recipe/views.py
+++ recipe/views.py
@@ -85,7 +85,17 @@ class RecipeViewSets(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            ),
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
                  mixins.UpdateModelMixin, 
                  mixins.ListModelMixin, 
@@ -97,7 +107,15 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
     """Filter queryset to authenticated user."""
     def get_queryset(self):
         """Filter queryset to authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+            user=self.request.user
+            ).order_by('-name').distince()
 
 class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database."""
